3243e30fc2f56f18a7110b0eebff4543acb54c71
User <snehaladhav995@gmail.com>
2020-07-25 10:15:08 +0530

    Updated code

--- app/src/main/java/com/example/tellmesomethingapplication/MainActivity.java
+++ app/src/main/java/com/example/tellmesomethingapplication/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.tellmesomethingapplication;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -17,12 +20,28 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
     TextView voiceInput,speakBtn;
      final int REQ_SPEECH_CODE=100;
+     ToggleButton toggleButton;
+     ConstraintLayout constraintLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         voiceInput=findViewById(R.id.voiceInput);
         speakBtn=findViewById(R.id.btnSpeak);
+        toggleButton=findViewById(R.id.toggleButton);
+        constraintLayout=findViewById(R.id.constrain);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(compoundButton.isChecked()){
+                    speakBtn.setBackground(getResources().getDrawable(android.R.drawable.alert_dark_frame));
+                    Toast.makeText(MainActivity.this, "ONN", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "OFF", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         speakBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             }
-                break;
+            break;
         }
     }
 }
